37e4ab94c62ed3874d68c279d8d8bfac36547533
7com <bl4ckt3ch@gmail.com>
2018-01-17 15:21:02 -0300

    Adaptación para Slidebase 17-01-2018

--- CapturaDatos.ino
+++ CapturaDatos.ino
@@ -4,12 +4,18 @@
 //conectados al Scorbot
 
 boolean enviar = false;
+boolean slidebase = false;
 
 void serialEvent() {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    if (inChar == '\n') {
+    if (inChar == '0') {
       enviar = true;
+      slidebase = false;
+    }
+    if (inChar == '1') {
+      enviar = true;
+      slidebase = true;
     }
   }
 }
@@ -20,23 +26,48 @@ void setup() {
 }
 
 void loop() {
-  //Se crean un arreglo de 6 Motor y se capturan los datos.
-  Motor m[6];
-  for (int i = 0; i < 6; i++)
+  //Se crean un arreglo de 7 Motor y se capturan los datos.
+  Motor m[7];
+  for (int i = 0; i < 7; i++)
   {
     m[i] = capturar(0 + i, 8 + i);
   }
 
   //Si se activo la bandera del evento serial, se inicia el envio de datos por serial
-  if (enviar)
+  if (enviar && !slidebase)
   {
-    String volt="";
-    String freq="";
-    String temp="";
+    String volt = "";
+    String freq = "";
+    String temp = "";
     //voltaje() + freq() + temp()
     for (int i = 0; i < 6; i++)
     {
       if (i == 5)
+      {
+        volt = volt + m[i].volt + ";";
+        freq = freq + m[i].freq + ";";
+        temp = temp + m[i].temp;
+      }
+      else
+      {
+        volt = volt + m[i].volt + ",";
+        freq = freq + m[i].freq + ",";
+        temp = temp + m[i].temp + ",";
+      }
+    }
+    String s = volt + freq + temp + "#\r";
+    Serial.print(s);
+    enviar = false;
+  }
+  if (enviar && slidebase)
+  {
+    String volt = "";
+    String freq = "";
+    String temp = "";
+    //voltaje() + freq() + temp()
+    for (int i = 0; i < 7; i++)
+    {
+      if (i == 6)
       {
         volt = volt + m[i].volt + ";";
         freq = freq + m[i].freq + ";";
@@ -76,9 +107,9 @@ Motor capturar(int pinE, int pinT)
   }
   else
   {
-    hz=-1;
+    hz = -1;
   }
-  
+
 
   if (high == 0 || low == 0) {
     volt = 12;
